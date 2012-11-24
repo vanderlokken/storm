450831aac62317fd94ba0bf98b1c9975742b1d06
vanderlokken <vanderlokken@gmail.com>
2012-11-25 01:26:25 +0400

    Implemented the 'Clock' class using C++11 '<chrono>' library

---
+++ src/HighPrecisionClock.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+
+#include "Clock.h"
+
+namespace storm {
+    
+class HighPrecisionClock : public Clock {
+public:
+    HighPrecisionClock();
+
+    virtual void update() noexcept;
+    
+    virtual Time getTime() const noexcept;
+    virtual Time getTimeChange() const noexcept;
+
+private:
+    std::chrono::high_resolution_clock::time_point _start;
+    std::chrono::high_resolution_clock::time_point _timePoint;
+    std::chrono::high_resolution_clock::duration _timeChange;
+};
+
+}
