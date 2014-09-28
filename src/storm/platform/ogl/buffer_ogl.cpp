a979bde8b992f4f3e4dcef6afeba65e57945fb74
vanderlokken <vanderlokken@gmail.com>
2014-09-28 13:46:11 +0400

    Updated BufferOgl implementation

--- src/storm/platform/ogl/buffer_ogl.cpp
+++ src/storm/platform/ogl/buffer_ogl.cpp
@@ -1,7 +1,5 @@
 #include <storm/platform/ogl/buffer_ogl.h>
 
-#include <cstring>
-
 #include <storm/platform/ogl/check_result_ogl.h>
 #include <storm/platform/ogl/rendering_system_ogl.h>
 #include <storm/platform/ogl/resource_type_ogl.h>
@@ -11,19 +9,18 @@ namespace storm {
 BufferHandleOgl::BufferHandleOgl() {
     ::glGenBuffers( 1, &_handle );
     checkResult( "::glGenBuffers" );
-    return;
 }
 
 BufferHandleOgl::~BufferHandleOgl() {
     ::glDeleteBuffers( 1, &_handle );
-    return;
 }
 
-// GL_COPY_WRITE_BUFFER and GL_COPY_READ_BUFFER are not used in the rendering process
-// so it's safe to overwrite these targets and to use them in the data transfer operations.
+// GL_COPY_WRITE_BUFFER and GL_COPY_READ_BUFFER are not used in the rendering
+// process so it's safe to overwrite these targets and to use them in the data
+// transfer operations.
 
-BufferOgl::BufferOgl( const Description &description, const void *data )
-    : _description( description )
+BufferOgl::BufferOgl( const Description &description, const void *data ) :
+    _description( description )
 {
     ::glBindBuffer( GL_COPY_WRITE_BUFFER, _handle );
     checkResult( "::glBindBuffer" );
@@ -32,8 +29,6 @@ BufferOgl::BufferOgl( const Description &description, const void *data )
 
     ::glBufferData( GL_COPY_WRITE_BUFFER, _description.size, data, usage );
     checkResult( "::glBufferData" );
-
-    return;
 }
 
 void BufferOgl::getData( size_t offset, size_t size, void *data ) const {
@@ -42,7 +37,6 @@ void BufferOgl::getData( size_t offset, size_t size, void *data ) const {
 
     ::glGetBufferSubData( GL_COPY_READ_BUFFER, offset, size, data );
     checkResult( "::glGetBufferSubData" );
-    return;
 }
 
 void BufferOgl::setData( size_t offset, size_t size, const void *data ) {
@@ -51,17 +45,8 @@ void BufferOgl::setData( size_t offset, size_t size, const void *data ) {
     ::glBindBuffer( GL_COPY_WRITE_BUFFER, _handle );
     checkResult( "::glBindBuffer" );
 
-    void *destination = ::glMapBufferRange( GL_COPY_WRITE_BUFFER, offset, size, GL_MAP_WRITE_BIT );
-    checkResult( "::glMapBufferRange" );
-
-    ::memcpy( destination, data, size );
-
-    ::glUnmapBuffer( GL_COPY_WRITE_BUFFER );
-    checkResult( "::glUnmapBuffer" );
-
-    // glUnmapBuffer can return 'false' indicating the data corruption.
-    // Now the return value is simply ignored.
-    return;
+    ::glBufferSubData( GL_COPY_WRITE_BUFFER, offset, size, data );
+    checkResult( "::glBufferSubData" );
 }
 
 const Buffer::Description& BufferOgl::getDescription() const {
